c2a99bb24a702e044997efb1d2e635363ab02363
michele <michele@moon>
2019-02-04 10:33:11 +0200

    General update upon internal meeting agreement.

--- main.cpp
+++ main.cpp
@@ -5,18 +5,13 @@ int main(int argc, char *argv[])
 {
     QCoreApplication a(argc, argv);
     int stam=2000;
-    AlgNotificationTypes stam2=AlgModStartedForConstruction;
+    AlgBuildingNotificationTypes stam2=AlgModInit;
     ManagementAlgorithm ma;
-    ma.Cons_InitiateSensorLocationReporting(stam);
+    ma.Cons_InitiateSensorLocationReporting(stam, stam);
     ma.ProcessSensorLocUpdate(stam);
-    ma.LoadOrthophoto(stam);
-    ma.LoadWPSModel(stam);
     ma.Cons_PrepareForModelConstruction(stam, stam);
     ma.ProcessNotification(stam2);
-    ma.QueryCloud(stam, stam);
     ma.Cons_StopModelConstruction();
-    ma.StopUdatingLoc();
-    //ma.Al_StoreWPS_ModelData(stam);
-    //ma.Al_UploadWPSData(stam);
+    ma.StopUdatingLocation();
     return a.exec();
 }
